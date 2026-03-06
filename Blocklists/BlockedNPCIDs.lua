42bed98aeb610e8625ccc46d1bef4cb8f63221c8
Kyle Ratliff <kylecrat@me.com>
2026-03-06 15:31:11 -0500

    Block NPC automation for the Soiree

--- Blocklists/BlockedNPCIDs.lua
+++ Blocklists/BlockedNPCIDs.lua
@@ -4,5 +4,18 @@ local _, AQG = ...
 AQG.BlockedNPCIDs = {
     -- NPC Name (Zone)
     -- Reason: why this NPC is blocked
-    -- [123456],
+    -- [123456] = true,
+    --
+    ----------------------------------------------------------------------------
+    -- Saltheril's Soiree NPC's
+    -- (Eversong Woods, Fairbreeze Village)
+    --
+    -- Ranger-Captain Dawnfletch
+    [240836] = true,
+    -- Magistrix Bloodflame
+    [240834] = true,
+    -- Knight-Lord Sunguard
+    [240835] = true,
+    -- Vyrin the Supplier
+    [240837] = true,
 }
